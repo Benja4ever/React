4c4f5579603ddb585baf61cc78f4b9be7fb2ec22
Benja4ever <149781728+Benja4ever@users.noreply.github.com>
2025-01-15 14:09:58 +0100

    dos 15-01

--- mujeresTresC/src/componentes/seccion5.tsx
+++ mujeresTresC/src/componentes/seccion5.tsx
@@ -3,6 +3,7 @@ const Section5 = () =>{
     return (
         <section className="seccion5">
             <article className="section5-article">
+                <h3>Nuestras Actividades</h3>
                 <div>
                     <ul className="container">
                         <li>
@@ -41,8 +42,38 @@ const Section5 = () =>{
                                 </span>
                             </div>
                         </li>
+                        <li>
+                            <img src='https://2.bp.blogspot.com/-jrAzEzpCQN4/UhfKSzqeZ5I/AAAAAAAAAKg/wzzEZmt7CnE/s1600/LONDRES.jpg' alt="" />
+                            <div className="content">
+                                <span>
+                                    <h2><a href="">London</a></h2>
+                                    <p>Capital de Inglaterra y Reino Unido, ubicada en Europa y jskhdbvkjdbvjkhdfbvjhdfbvjh jhbvjhkd jhbvjh jhbdcjkhad jhjhbjhkbc jkhbvjkhbdc </p>
+                                </span>
+                            </div>
+                        </li>
+                        <li>
+                            <img src='https://2.bp.blogspot.com/-jrAzEzpCQN4/UhfKSzqeZ5I/AAAAAAAAAKg/wzzEZmt7CnE/s1600/LONDRES.jpg' alt="" />
+                            <div className="content">
+                                <span>
+                                    <h2><a href="">London</a></h2>
+                                    <p>Capital de Inglaterra y Reino Unido, ubicada en Europa y jskhdbvkjdbvjkhdfbvjhdfbvjh jhbvjhkd jhbvjh jhbdcjkhad jhjhbjhkbc jkhbvjkhbdc </p>
+                                </span>
+                            </div>
+                        </li>
                     </ul>
-                </div>   
+                </div> 
+                <div className="section5-button">
+                    <div className="button-wrapper">
+                        <button className="btn-5">
+                            <span>Ver más</span>
+                        </button>
+                    </div>
+                    <div className="section5-button">
+                        <button className="btn-5">
+                            <span>Hazte socia</span>
+                        </button>
+                    </div>
+            </div>
             </article>
         </section>
     );
