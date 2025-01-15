1462b9c901ec0f00f456781bb1b16466d4083eba
Benja4ever <149781728+Benja4ever@users.noreply.github.com>
2025-01-15 12:40:06 +0100

    uno 15-03

--- mujeresTresC/src/componentes/seccion5.tsx
+++ mujeresTresC/src/componentes/seccion5.tsx
@@ -1,60 +1,48 @@
-import './css/seccion5.css';
+import './css/section5.css';
 const Section5 = () =>{
     return (
         <section className="seccion5">
-            <article className="articulo-seccion5">
-                <div className="contenedor-imagenes">
+            <article className="section5-article">
+                <div>
                     <ul className="container">
                         <li>
-                            <img
-                                src="https://2.bp.blogspot.com/-jrAzEzpCQN4/UhfKSzqeZ5I/AAAAAAAAAKg/wzzEZmt7CnE/s1600/LONDRES.jpg"
-                                alt="London"
-                            />
+                            <img src='https://2.bp.blogspot.com/-jrAzEzpCQN4/UhfKSzqeZ5I/AAAAAAAAAKg/wzzEZmt7CnE/s1600/LONDRES.jpg' alt="" />
                             <div className="content">
                                 <span>
                                     <h2><a href="">London</a></h2>
-                                    <p>Capital de Inglaterra y Reino Unido, ubicada en Europa.</p>
+                                    <p>Capital de Inglaterra y Reino Unido, ubicada en Europa y jskhdbvkjdbvjkhdfbvjhdfbvjh jhbvjhkd jhbvjh jhbdcjkhad jhjhbjhkbc jkhbvjkhbdc </p>
                                 </span>
                             </div>
                         </li>
                         <li>
-                            <img
-                                src="https://2.bp.blogspot.com/-jrAzEzpCQN4/UhfKSzqeZ5I/AAAAAAAAAKg/wzzEZmt7CnE/s1600/LONDRES.jpg"
-                                alt="London"
-                            />
+                            <img src='https://2.bp.blogspot.com/-jrAzEzpCQN4/UhfKSzqeZ5I/AAAAAAAAAKg/wzzEZmt7CnE/s1600/LONDRES.jpg' alt="" />
                             <div className="content">
                                 <span>
                                     <h2><a href="">London</a></h2>
-                                    <p>Capital de Inglaterra y Reino Unido, ubicada en Europa.</p>
+                                    <p>Capital de Inglaterra y Reino Unido, ubicada en Europa y jskhdbvkjdbvjkhdfbvjhdfbvjh jhbvjhkd jhbvjh jhbdcjkhad jhjhbjhkbc jkhbvjkhbdc </p>
                                 </span>
                             </div>
                         </li>
                         <li>
-                            <img
-                                src="https://2.bp.blogspot.com/-jrAzEzpCQN4/UhfKSzqeZ5I/AAAAAAAAAKg/wzzEZmt7CnE/s1600/LONDRES.jpg"
-                                alt="London"
-                            />
+                            <img src='https://2.bp.blogspot.com/-jrAzEzpCQN4/UhfKSzqeZ5I/AAAAAAAAAKg/wzzEZmt7CnE/s1600/LONDRES.jpg' alt="" />
                             <div className="content">
                                 <span>
                                     <h2><a href="">London</a></h2>
-                                    <p>Capital de Inglaterra y Reino Unido, ubicada en Europa.</p>
+                                    <p>Capital de Inglaterra y Reino Unido, ubicada en Europa y jskhdbvkjdbvjkhdfbvjhdfbvjh jhbvjhkd jhbvjh jhbdcjkhad jhjhbjhkbc jkhbvjkhbdc </p>
                                 </span>
                             </div>
                         </li>
                         <li>
-                            <img
-                                src="https://2.bp.blogspot.com/-jrAzEzpCQN4/UhfKSzqeZ5I/AAAAAAAAAKg/wzzEZmt7CnE/s1600/LONDRES.jpg"
-                                alt="London"
-                            />
+                            <img src='https://2.bp.blogspot.com/-jrAzEzpCQN4/UhfKSzqeZ5I/AAAAAAAAAKg/wzzEZmt7CnE/s1600/LONDRES.jpg' alt="" />
                             <div className="content">
                                 <span>
                                     <h2><a href="">London</a></h2>
-                                    <p>Capital de Inglaterra y Reino Unido, ubicada en Europa.</p>
+                                    <p>Capital de Inglaterra y Reino Unido, ubicada en Europa y jskhdbvkjdbvjkhdfbvjhdfbvjh jhbvjhkd jhbvjh jhbdcjkhad jhjhbjhkbc jkhbvjkhbdc </p>
                                 </span>
                             </div>
                         </li>
                     </ul>
-                </div>
+                </div>   
             </article>
         </section>
     );
